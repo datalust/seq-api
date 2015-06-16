ce760236b64849b9a5a50311389b182fa0431313
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2015-06-16 20:18:56 +1000

    Update events resource group to use `afterId` and `startAtId` in line with the 2.0 API.

--- src/Seq.Api/Api/ResourceGroups/EventsResourceGroup.cs
+++ src/Seq.Api/Api/ResourceGroups/EventsResourceGroup.cs
@@ -32,8 +32,8 @@ namespace Seq.Api.ResourceGroups
             var parameters = new Dictionary<string, object>();
             if (filter != null) { parameters.Add("filter", filter); }
             if (count != null) { parameters.Add("count", count.Value); }
-            if (start != null) { parameters.Add("start", start); }
-            if (after != null) { parameters.Add("after", after); }
+            if (start != null) { parameters.Add("startAtId", start); }
+            if (after != null) { parameters.Add("afterId", after); }
             if (render) { parameters.Add("render", true); }
             if (fromDateUtc != null) { parameters.Add("fromDateUtc", fromDateUtc.Value); }
             if (toDateUtc != null) { parameters.Add("toDateUtc", toDateUtc.Value); }
@@ -58,8 +58,8 @@ namespace Seq.Api.ResourceGroups
             if (intersectIds != null && intersectIds.Length > 0) { parameters.Add("intersectIds", string.Join(",", intersectIds)); }
             if (filter != null) { parameters.Add("filter", filter); }
             if (count != null) { parameters.Add("count", count.Value); }
-            if (start != null) { parameters.Add("start", start); }
-            if (after != null) { parameters.Add("after", after); }
+            if (start != null) { parameters.Add("startAtId", start); }
+            if (after != null) { parameters.Add("afterId", after); }
             if (render) { parameters.Add("render", true); }
             if (fromDateUtc != null) { parameters.Add("fromDateUtc", fromDateUtc.Value); }
             if (toDateUtc != null) { parameters.Add("toDateUtc", toDateUtc.Value); }
@@ -85,8 +85,8 @@ namespace Seq.Api.ResourceGroups
             var parameters = new Dictionary<string, object> { { "intersectIds", string.Join(",", intersectIds) } };
             if (filter != null) { parameters.Add("filter", filter); }
             if (count != null) { parameters.Add("count", count.Value); }
-            if (start != null) { parameters.Add("start", start); }
-            if (after != null) { parameters.Add("after", after); }
+            if (start != null) { parameters.Add("startAtId", start); }
+            if (after != null) { parameters.Add("afterId", after); }
             if (render) { parameters.Add("render", true); }
             if (fromDateUtc != null) { parameters.Add("fromDateUtc", fromDateUtc.Value); }
             if (toDateUtc != null) { parameters.Add("toDateUtc", toDateUtc.Value); }
