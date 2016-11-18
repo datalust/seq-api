35757e6d3d46fc43b8c94d7dac8c4026fecc3fb5
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2016-11-18 14:23:00 +1000

    Add all supported Seq 3.4 setting names

---
+++ src/Seq.Api/Api/Model/Settings/SettingName.cs
@@ -0,0 +1,26 @@
+﻿namespace Seq.Api.Model.Settings
+{
+    public enum SettingName
+    {
+        AutomaticAccessADGroup,
+        AutomaticallyGrantUserAccessToADAccounts,
+        AzureADClientId,
+        AzureADClientKey,
+        AzureADTenantId,
+        BackupLocation,
+        BackupsToKeep,
+        BackupUtcTimeOfDay,
+        CheckForPackageUpdates,
+        CheckForUpdates,
+        InstanceTitle,
+        IsActiveDirectoryAuthentication,
+        IsAuthenticationEnabled,
+        LazilyFlushEventWrites,
+        MasterKeyIsBackedUp,
+        MinimumFreeStorageSpace,
+        RequireApiKeyForWritingEvents,
+        RawEventMaximumContentLength,
+        RawPayloadMaximumContentLength,
+        ThemeStyles
+    }
+}
