f1ec6e67c82360b0889e5374e4395e9dbd62bdeb
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-12-07 13:09:42 +1000

    Only ignore backups created in the root of the project

---
+++ src/Seq.Api/Model/Backups/BackupEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Seq.Api.Model.Backups
+{
+    public class BackupEntity : Entity
+    {
+        public string CreatedAt { get; set; }
+        public string Filename { get; set; }
+        public long FileSizeBytes { get; set; }
+    }
+}
