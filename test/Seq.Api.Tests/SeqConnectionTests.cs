e9d71aa6f34756b45f612799c75ffbee2bd87bba
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-03-22 12:55:50 +1000

    Suppress warning in test for deprecated API

--- test/Seq.Api.Tests/SeqConnectionTests.cs
+++ test/Seq.Api.Tests/SeqConnectionTests.cs
@@ -9,10 +9,12 @@ namespace Seq.Api.Tests
         {
             var callCount = 0;
 
+#pragma warning disable CS0618 // Type or member is obsolete
             using var _ = new SeqConnection("https://test.example.com", null, handler => { 
                 Assert.NotNull(handler);
                 ++callCount;
             });
+#pragma warning restore CS0618 // Type or member is obsolete
 
             Assert.Equal(1, callCount);
         }
